e4268fa861b29e56a52470cf51e0dc45e14f0cd7
matash123 <43766822+matash123@users.noreply.github.com>
2022-02-08 23:50:34 -0700

    changes13

--- import.py
+++ import.py
@@ -1,18 +1,16 @@
-import psycopg2
+import os
 import csv
 
 conn = psycopg2.connect("host=ec2-52-0-67-144.compute-1.amazonaws.com dbname=d9rbgsmkbh5270 user=ugobzqnheqklpw password=3b428743e124dd0a1f02b89a0373208cd7e7ca39d980d6e0d97b2a7aeae34361")
 cur = conn.cursor()
-cur.execute('''CREATE TABLE bookssss(
-        isbn integer PRIMARY KEY,
-        title text,
-        author text,
-        year integer
-)
-''')
 conn.commit()
 with open('books.csv', 'r') as f:
+    reader = csv.reader(f)
     next(f)
-    cur.copy_from(f, 'bookssss', sep=';')
+    for isbn, title, author, year in reader:
+        conn.execute ("INSERT INTO booksssss (isbn, title, author, year \
+            VALUES (:isbn, :title, :author, :year)", \
+                {"isbn":isbn, "title":title, "author":author, "year":year})
 
 conn.commit()
+
